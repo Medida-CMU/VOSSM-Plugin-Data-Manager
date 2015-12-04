717d99fa2138ff1c6d50036b6dd81530eb23d15b
preethibaskar <preethibaskar90@gmail.com>
2015-12-03 23:05:30 -0500

    Updated code for filtering and aggregating data

--- VOSSM-Data-Collector-Parser/parse.py
+++ VOSSM-Data-Collector-Parser/parse.py
@@ -1,6 +1,6 @@
 # Parse.py parses the json data and writes them to DB.
 # Once written, the file is moved to parsed_db folder
-
+import time
 import json
 import os
 import shutil
@@ -9,16 +9,28 @@ from pymongo import MongoClient
 client = MongoClient();
 db = client['VOSSM'];
 collection = db['raw_data']
+parsed_collection = db['parsed_data']
 def parseFile(filename):
  data = []
+ selected_data = {}
  with open(filename) as f:
     for line in f:
         #data.append(json.loads(line))
+
 	data = json.loads(line)
 	new_json = json.loads(data[0], object_hook=remove_dot_key)
-	collection.insert(new_json)
-	print(new_json)
-
+	selected_data['version'] =new_json['scimapInfoVersion']
+	selected_data['hardware'] =new_json['platform']['hardware']
+	selected_data['system'] = new_json['platform']['system']
+	selected_data['user'] = new_json['user']
+	selected_data['start_time'] = new_json['startTime']
+	selected_data['end_time'] = new_json['endTime']
+	for key in new_json['pkgT']:
+		selected_data['_id'] = time.time()
+		selected_data['package'] = key
+		collection.insert(selected_data)
+	print(selected_data)
+	aggregateData()
  # src = "raw_data/"
  # dst = "parsed_data/"
  # for file in os.listdir(src):
@@ -37,5 +49,16 @@ def remove_dot_key(obj):
             del obj[key]
     return obj
 
+def aggregateData():
+	result = collection.aggregate([{"$group": {"_id" : 
+												{'package': "$package",
+												 'version' : "$version",
+												 'hardware' : "$hardware", 
+												 'system' : "$system"
+												}, 
+											"occurences": {"$sum": 1}
+											}},
+											{"$out" : "parsed_data"}
+								 ])
 
 #pprint(data[1]);
